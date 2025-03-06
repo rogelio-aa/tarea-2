562ebf71b3c1b83a0fa6587a98f9d4a4172a5de9
rogelio-aa <1573313@senati.pe>
2025-03-06 18:02:06 -0500

    Update tarea2.py

--- tarea2.py
+++ tarea2.py
@@ -1,10 +1,22 @@
 
+  
 
-import numpy as np 
+
+
+#      ejercicio 1
+
+
+palabra = input("Ingresa una palabra: ").lower()
+print(f"La letra 'h' se encuentra en la posición: {palabra.find('h')}" if 'h' in palabra else "La letra 'h' no se encuentra en la palabra.")
+
+
+
+#     ejercicio 2
+import numpy as np
 import matplotlib.pyplot as plt
 
-x = np.linspace(-2 * np.pi , 2 * np.pi, 1000 )
-plt.plot(x, np.sin(x), label='seno')
-plt.plot(x, np.cos(x), label='coseno')
+x = np.linspace(-2 * np.pi, 2 * np.pi, 1000)
+plt.plot(x, np.sin(x), color='yellow', label='Seno')
+plt.plot(x, np.cos(x), color='blue', linewidth=2, label='Coseno')
 plt.legend()
 plt.show()
